1506e2a34d9888da0150be25076be37e5f61e3a9
ChengXi <xcheng@linkernetworks.com>
2016-10-08 20:09:17 +0800

    modify start edit page

--- src/js/clickStart.js
+++ src/js/clickStart.js
@@ -1,13 +1,36 @@
 import {resizeWidget} from "./theme/widget";
+import {initActionIO,initTreeEdit,initFromEdit,initFromView} from "./action.io";
+
+var pipelineType = "";
 
 export function clickStart(sd, si) {
     //show git form
     $.ajax({
-        url: "./templates/gitEdit.html",
+        url: "./templates/startEdit.html",
         type: "GET",
         cache: false,
         success: function (data) {
             $("#pipeline-info-edit").html($(data));
+
+            // start output
+            initActionIO(sd);
+
+            // pipeline type
+            pipelineType = $("#type-select").val();
+            outputBaseOnType();
+            $("#type-select").on('change',function(){
+                pipelineType = $("#type-select").val();
+                outputBaseOnType();
+            })
+            
+            //output from edit
+            $("#tree-edit-tab").on('click',function(){
+                initTreeEdit();
+            })
+
+            $("#output-from-edit-tab").on('click',function(){
+                initFromEdit("output");
+            });
 
             $.each(sd.setupData, function (name, value) {
                 console.log($("#" + name));
@@ -19,4 +42,17 @@ export function clickStart(sd, si) {
             resizeWidget();
         }
     });
+}
+
+function outputBaseOnType(){
+    if(pipelineType == "github" || pipelineType == "gitlab"){
+        $("#outputTreeViewer").show();
+        $("#outputTreeDesigner").hide();
+        initFromView();
+    }else{
+        $("#outputTreeViewer").hide();
+        $("#outputTreeDesigner").show();
+        initTreeEdit();
+        initFromEdit("output");
+    }
 }
